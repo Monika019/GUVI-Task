27996d810dc6f2719d0e5b0934e2928141ffa3d3
MONIKA K T <125746938+Monika019@users.noreply.github.com>
2023-11-23 20:21:07 +0530

    Add files via upload

---
+++ php/login.php
@@ -0,0 +1,31 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "user_auth";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$sql = "SELECT * FROM users WHERE username = '$username'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    if (password_verify($password, $row['password'])) {
+        header("Location: profile.php");
+    } else {
+        echo "Invalid password";
+    }
+} else {
+    echo "User not found";
+}
+
+$conn->close();
+?>
---
+++ php/register.php
@@ -0,0 +1,25 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "user_auth";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$username = $_POST['username'];
+$password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+$sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+
+if ($conn->query($sql) === TRUE) {
+    header("Location: profile.php");
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
